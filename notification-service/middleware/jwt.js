73e94cb78976163787833b90c299770ec52aaf46
SpoooKyHammer <spookyhammer2300@gmail.com>
2024-07-07 12:16:10 +0530

    update: attr `userId` changed to `user`

--- notification-service/middleware/jwt.js
+++ notification-service/middleware/jwt.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded;
+    req.user = decoded;
     next();
   } catch (error) {
     return res.status(401).json({ error: 'Unauthorized - Invalid token' });
