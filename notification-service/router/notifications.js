3e202d074f06f7c68f0db3c8b7ed1b7c743d3ed7
SpoooKyHammer <spookyhammer2300@gmail.com>
2024-07-07 12:18:50 +0530

    added get endpoints

--- notification-service/router/notifications.js
+++ notification-service/router/notifications.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { isUUID } = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -6,8 +7,19 @@ const { notificationsModel } = require("./../schema/notification");
 
 const notificationRouter = Router();
 
-notificationRouter.get("/", (req, res) => {
-  res.send("hi")
+notificationRouter.get("/", async (req, res) => {
+  const notifications = await notificationsModel.find({_id: req.user.userId});
+  res.json(notifications);
+})
+
+notificationRouter.get("/:id", async (req, res) => {
+  if (!isUUID(req.params.id)) return res.status(422).json({error: "id must be a valid UUID."});
+
+  const notification = await notificationsModel.findOne({_id: req.params.id});
+
+  if (notification) return res.json(notification);
+
+  res.status(404).json({error: "Notification not found with the requested id."})
 })
 
 module.exports = notificationRouter;
